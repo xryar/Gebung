aa5dc64faccca30dd8805bc947fc11e29b0924f5
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-05-27 20:24:24 +0700

    membuat tampilan edit profile, About dan Custom Dialog

---
+++ app/src/main/java/com/example/gebung/ui/editprofile/EditProfileActivity.kt
@@ -0,0 +1,22 @@
+package com.example.gebung.ui.editprofile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.gebung.R
+import com.example.gebung.databinding.ActivityEditProfileBinding
+
+class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityEditProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
